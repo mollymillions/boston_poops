27d764446b373e7fe8d47a6e3be3f82c593ce6f1
mollymillions <alewis13@bu.edu>
2013-05-01 01:05:06 -0400

    completed file io in location_test file

--- Location.java
+++ Location.java
@@ -4,7 +4,6 @@ import java.lang.*;
 
 public class Location {
 	
-<<<<<<< HEAD
 	/*List of functions:
 	 * void setHours()
 	 * void setRating()
@@ -21,53 +20,39 @@ public class Location {
 	private String address;
 	private double latitude;
 	private double longitude;
-=======
-	private String buildingName;
-	private String address;
-	private int latitude;
-	private int longitude;
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
-	private int[] open;
-	private int[] close;
+	private String[] open;
+	private String[] close;
 	private int rating;
+	private int mapID;
 
 	//Constructor
 	//Default constructor
 	public Location() {
 		this.buildingName = "";
 		this.address = "";
-<<<<<<< HEAD
 		this.latitude = 0.0;
 		this.longitude = 0.0;
-=======
-		this.latitude = 0;
-		this.longitude = 0;
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
 		this.rating = 0;
-		
-		this.open = new int[7];
-		this.close = new int[7];
+		this.mapID = 0;
+		this.open = new String[7];
+		this.close = new String[7];
 	}
 	
 	//Constructor with input
-<<<<<<< HEAD
 	public Location(String name, String address, double latitude, double longitude, int rating) {
-=======
-	public Location(String name, String address, int latitude, int longitude, int rating) {
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
 		this.buildingName = name;
 		this.address = address;
 		this.latitude = latitude;
 		this.longitude = longitude;
 		this.rating = rating;
-		
-		this.open = new int[7];
-		this.close = new int[7];
+		this.mapID = 0;
+		this.open = new String[7];
+		this.close = new String[7];
 	}
 	
 	//function that sets when the place is open - takes two arrays as inputs
-	public void setHours(int[] o_hours, int[] c_hours) {
-		for (int i=0; i<6; i++) {
+	public void setHours(String[] o_hours, String[] c_hours) {
+		for (int i=0; i<7; i++) {
 			this.open[i] = o_hours[i];
 			this.close[i] = c_hours[i];
 		}
@@ -84,7 +69,6 @@ public class Location {
 	}
 	
 	//function that returns the latitude
-<<<<<<< HEAD
 	public double getLatitude() {
 		return this.latitude;
 	}
@@ -95,58 +79,41 @@ public class Location {
 	}
 	
 	//function that checks the hours to determine if the location is open
-	public boolean isOpen(int day, int hour, int minute) {
+	public boolean isOpen(int day, String hour, String minute) {
+		int int_close = 0;
+		int int_open = 0;
+		int_close = Integer.valueOf(close[day]);
+		int_open = Integer.valueOf(open[day]);
+		
 		int currentTime = Integer.valueOf(hour+""+minute);
-		if ((currentTime-open[day])<0) {
+		
+		if ((currentTime-int_open)<0) {
 			return false;
 		}
-		if ((currentTime-close[day])>0) {
-=======
-	public int getLatitude() {
-		return this.latitude;
-	}
-	
-	public int getLongitude() {
-		return this.longitude;
-	}
-	
-	public boolean isOpen(int day, int hour, int minute) {
-		int currentTime = Integer.valueOf(hour+""+minute);
-		if ((currentTime-open[day])<0) {
-			System.out.println(currentTime-open[day]);
-			return false;
-		}
-		if ((currentTime-close[day])<0) {
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
+		if ((currentTime-int_close)>0) {
 			return false;
 		}
 		return true;
 	}
 	
-<<<<<<< HEAD
 	//function that checks the hours to see if it closes within a half hour
-=======
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
-	public boolean closeSoon(int day, int hour, int minute) {
+	public boolean closeSoon(int day, String hour, String minute) {
+		int int_close = 0;
+		int_close = Integer.valueOf(close[day]);		
 		int currentTime = Integer.valueOf(hour+""+minute);
-		if ((0 < (close[day]-currentTime)) && ((close[day]-currentTime) < 30)) {
+		
+		if ((0 < (int_close-currentTime)) && ((int_close-currentTime) < 30)) {
 			return true;
 		}
 		return false;
 	}
 	
-<<<<<<< HEAD
 	//returns the rating
-=======
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
 	public int getRating() {
 		return this.rating;
 	}
 	
-<<<<<<< HEAD
 	//sets the rating (this is the only mutator so far)
-=======
->>>>>>> a9db1e3e4e7dcc51e6b2fc10e804645ad7d426ce
 	public void setRating(int set_rating) {
 		if (0<=set_rating && set_rating<=5) {
 			this.rating = set_rating;
--- location_test.java
+++ location_test.java
@@ -1,5 +1,7 @@
 package boston_poops;
 
+import java.util.*; //need for Scanner
+import java.io.*; //need for file operations
 import java.lang.*;
 
 public class location_test {
@@ -7,17 +9,68 @@ public class location_test {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//make a location
-		Location South_Station = new Location("South Station","700 Atlantic Avenue",0,0,0);
-		int[] open = {800,800,800,800,800,800,800};
-		int[] close ={800,800,800,800,800,800,800};
-		South_Station.setHours(open, close);
+	//This method counts the number of lines in a file
+	public static int getLines(String filename) throws FileNotFoundException {
+		Scanner input = new Scanner(new File(filename));
+		int count = 0;
 		
-		System.out.println(South_Station.getRating());
-		System.out.println(South_Station.isOpen(0, 17, 44));
+		while (input.hasNextLine()) {
+			input.nextLine();
+			count++;
+		}
+		input.close();
+		return count;
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		//make an array of locations - this requires us to know how many locations we have
+		
+		//THIS IS IMPORTANT - this is where the program takes in all the input
+		String filename = "locations_list.csv";
+		int num_lines = getLines(filename);
+		//initialize the array that the Locations will all be stored in		
+		Location[] places = new Location[num_lines];
+		//while the file has lines to read in, read them in!
+		int iterator = 0;
+		Scanner input = new Scanner(new File(filename));
+		while (iterator<num_lines) {
+			input.useDelimiter(",|\r\n|\r|\n");
+			String address = input.next();
+			String buildingName = input.next();
+			String[] open = new String[7];
+			String[] close = new String[7];
+			for (int day = 0; day < 7; day++) {
+				open[day] = Integer.toString(input.nextInt());
+				close[day] = Integer.toString(input.nextInt());
+			}
+			double latitude = input.nextDouble();
+			double longitude = input.nextDouble();
+			
+			places[iterator] = new Location(buildingName,address,latitude,longitude,0);
+			places[iterator].setHours(open, close);
+			iterator++;
+		}
+		input.close();
 
+		//Check to see which places close within 30 minutes, and which ones are open
+		//I used the current date/time to check the status. If this was an android,
+		//we would call another method
+		//So we'd use the results of that in these variables
+
+		int current_day = 5;
+		String current_hour = "10";
+		String current_minute = "00";
+		for (int i=0; i < places.length; i++) {
+			if (places[i].closeSoon(current_day, current_hour, current_minute)) {
+				System.out.println("Close soon");
+			} else if (places[i].isOpen(current_day, current_hour, current_minute)) {
+				System.out.println("Is Open");
+			} else {
+				System.out.println("Closed");
+			}
+		}
+		
 	}
 
 }
